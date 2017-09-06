91d88c449736ff3ab40b37e58571422c99e97acb
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-09-06 16:22:24 -0600

    progress on root page styling

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,29 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+      { test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        loader: 'file-loader',
+        options: {
+          query: {
+            name:'assets/[name].[ext]'
+          }
+        }
+      },
+      { loader: 'image-webpack-loader',
+        options: {
+          query: {
+            mozjpeg: {
+              progressive: true,
+            },
+            gifsicle: {
+              interlaced: true,
+            },
+            optipng: {
+              optimizationLevel: 7,
+            }
+          }
+        }
+      }
     ]
   },
   resolve: {
